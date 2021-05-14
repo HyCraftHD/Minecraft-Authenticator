fc87ce8759a64ab0f28132d3ef9b0b0edbda2aa9
HyCraftHD <hycrafthd@live.de>
2021-05-14 19:33:41 +0200

    Update service now requestOAuthAuthorizationToken is ready

--- src/main/java/net/hycrafthd/minecraft_authenticator/microsoft/MicrosoftService.java
+++ src/main/java/net/hycrafthd/minecraft_authenticator/microsoft/MicrosoftService.java
@@ -1,14 +1,34 @@
 package net.hycrafthd.minecraft_authenticator.microsoft;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Optional;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import net.hycrafthd.minecraft_authenticator.Constants;
+import net.hycrafthd.minecraft_authenticator.microsoft.api.OAuthErrorResponse;
+import net.hycrafthd.minecraft_authenticator.microsoft.api.OAuthTokenResponse;
 import net.hycrafthd.minecraft_authenticator.util.ConnectionUtil;
 import net.hycrafthd.minecraft_authenticator.util.HttpResponse;
 import net.hycrafthd.minecraft_authenticator.util.Parameters;
 
 public class MicrosoftService {
+	
+	private static HttpResponse oAuthTokenServiceRequest(Parameters parameters) throws IOException {
+		return ConnectionUtil.urlEncodedRequest(ConnectionUtil.urlBuilder(Constants.MICROSOFT_OAUTH_SERVICE, Constants.MICROSOFT_OAUTH_ENDPOINT_TOKEN), parameters);
+	}
+	
+	private static Optional<OAuthErrorResponse> findOAuthError(String responseString) {
+		final JsonElement element = JsonParser.parseString(responseString);
+		if (element.isJsonObject() && element.getAsJsonObject().get("error") != null) {
+			return Optional.of(Constants.GSON.fromJson(responseString, OAuthErrorResponse.class));
+		} else {
+			return Optional.empty();
+		}
+	}
 	
 	public static URL generateOAuthLoginUrl() {
 		final Parameters parameters = Parameters.create() //
@@ -20,29 +40,31 @@ public class MicrosoftService {
 		try {
 			return ConnectionUtil.urlBuilder(Constants.MICROSOFT_OAUTH_SERVICE, Constants.MICROSOFT_OAUTH_ENDPOINT_AUTHORIZE, parameters);
 		} catch (MalformedURLException ex) {
-			return null;
+			throw new AssertionError("This url should never be malformed.");
 		}
 	}
 	
-	public static void requestOAuthAuthorizationToken(String authorizationCode) {
+	public static MicrosoftResponse<OAuthTokenResponse, OAuthErrorResponse> requestOAuthAuthorizationToken(String authorizationCode) {
 		final Parameters parameters = Parameters.create() //
 				.add("client_id", Constants.MICROSOFT_CLIENT_ID) //
 				.add("code", authorizationCode) //
 				.add("grant_type", "authorization_code") //
 				.add("redirect_uri", Constants.MICROSOFT_OAUTH_REDIRECT_URL);
 		
+		final String responseString;
 		try {
-			final URL url = ConnectionUtil.urlBuilder(Constants.MICROSOFT_OAUTH_SERVICE, Constants.MICROSOFT_OAUTH_ENDPOINT_TOKEN);
-			
-			System.out.println(url);
-			
-			final HttpResponse response = ConnectionUtil.urlEncodedRequest(url, parameters);
-			
-			System.out.println(response.getResponseCode());
-			System.out.println(response.getAsString());
-		} catch (Exception ex) {
-			ex.printStackTrace();
+			responseString = oAuthTokenServiceRequest(parameters).getAsString();
+		} catch (IOException ex) {
+			return MicrosoftResponse.ofException(ex);
 		}
+		
+		final Optional<OAuthErrorResponse> errorResponse = findOAuthError(responseString);
+		if (errorResponse.isPresent()) {
+			return MicrosoftResponse.ofError(errorResponse.get());
+		}
+		
+		final OAuthTokenResponse response = Constants.GSON.fromJson(responseString, OAuthTokenResponse.class);
+		return MicrosoftResponse.ofResponse(response);
 	}
 	
 }
