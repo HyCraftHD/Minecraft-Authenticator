c5291ee68d5b055a546bb0f023d7e854af8a4723
HyCraftHD <hycrafthd@live.de>
2021-05-14 19:57:29 +0200

    Add payload and response for XBL

---
+++ src/main/java/net/hycrafthd/minecraft_authenticator/microsoft/api/XBLAuthenticatePayload.java
@@ -0,0 +1,69 @@
+package net.hycrafthd.minecraft_authenticator.microsoft.api;
+
+import com.google.gson.annotations.SerializedName;
+
+public class XBLAuthenticatePayload {
+	
+	@SerializedName("Properties")
+	private final Properties properties;
+	@SerializedName("RelyingParty")
+	private final String relyingParty;
+	@SerializedName("TokenType")
+	private final String tokenType;
+	
+	public XBLAuthenticatePayload(Properties properties, String relyingParty, String tokenType) {
+		this.properties = properties;
+		this.relyingParty = relyingParty;
+		this.tokenType = tokenType;
+	}
+	
+	public Properties getProperties() {
+		return properties;
+	}
+	
+	public String getRelyingParty() {
+		return relyingParty;
+	}
+	
+	public String getTokenType() {
+		return tokenType;
+	}
+	
+	@Override
+	public String toString() {
+		return "XBoxLiveAuthenticatePayload [properties=" + properties + ", relyingParty=" + relyingParty + ", tokenType=" + tokenType + "]";
+	}
+	
+	public static class Properties {
+		
+		@SerializedName("AuthMethod")
+		private final String authMethod;
+		@SerializedName("SiteName")
+		private final String siteName;
+		@SerializedName("RpsTicket")
+		private final String rpsTicket;
+		
+		public Properties(String authMethod, String siteName, String rpsTicket) {
+			this.authMethod = authMethod;
+			this.siteName = siteName;
+			this.rpsTicket = rpsTicket;
+		}
+		
+		public String getAuthMethod() {
+			return authMethod;
+		}
+		
+		public String getSiteName() {
+			return siteName;
+		}
+		
+		public String getRpsTicket() {
+			return rpsTicket;
+		}
+		
+		@Override
+		public String toString() {
+			return "Properties [authMethod=" + authMethod + ", siteName=" + siteName + ", rpsTicket=" + rpsTicket + "]";
+		}
+	}
+}
---
+++ src/main/java/net/hycrafthd/minecraft_authenticator/microsoft/api/XBLAuthenticateResponse.java
@@ -0,0 +1,81 @@
+package net.hycrafthd.minecraft_authenticator.microsoft.api;
+
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+public class XBLAuthenticateResponse {
+	
+	@SerializedName("IssueInstant")
+	private final String issueInstant;
+	@SerializedName("NotAfter")
+	private final String notAfter;
+	@SerializedName("Token")
+	private final String token;
+	@SerializedName("DisplayClaims")
+	private final DisplayClaims displayClaims;
+	
+	public XBLAuthenticateResponse(String issueInstant, String notAfter, String token, DisplayClaims displayClaims) {
+		this.issueInstant = issueInstant;
+		this.notAfter = notAfter;
+		this.token = token;
+		this.displayClaims = displayClaims;
+	}
+	
+	public String getIssueInstant() {
+		return issueInstant;
+	}
+	
+	public String getNotAfter() {
+		return notAfter;
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public DisplayClaims getDisplayClaims() {
+		return displayClaims;
+	}
+	
+	@Override
+	public String toString() {
+		return "XBLAuthenticateResponse [issueInstant=" + issueInstant + ", notAfter=" + notAfter + ", token=" + token + ", displayClaims=" + displayClaims + "]";
+	}
+	
+	public static class DisplayClaims {
+		
+		private final List<Xui> xui;
+		
+		public DisplayClaims(List<Xui> xui) {
+			this.xui = xui;
+		}
+		
+		public List<Xui> getXui() {
+			return xui;
+		}
+		
+		@Override
+		public String toString() {
+			return "DisplayClaims [xui=" + xui + "]";
+		}
+		
+		public static class Xui {
+			
+			private final String uhs;
+			
+			public Xui(String uhs) {
+				this.uhs = uhs;
+			}
+			
+			public String getUhs() {
+				return uhs;
+			}
+			
+			@Override
+			public String toString() {
+				return "Xui [uhs=" + uhs + "]";
+			}
+		}
+	}
+}
