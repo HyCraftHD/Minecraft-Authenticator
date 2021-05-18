63ff39eeb04046f213ec6532ac84abe375f2ce1b
HyCraftHD <hycrafthd@live.de>
2021-05-18 18:36:58 +0200

    Clear main to fix build

--- src/main/java/net/hycrafthd/minecraft_authenticator/Main.java
+++ src/main/java/net/hycrafthd/minecraft_authenticator/Main.java
@@ -1,11 +1,7 @@
 package net.hycrafthd.minecraft_authenticator;
 
-import net.hycrafthd.minecraft_authenticator.microsoft.MicrosoftService;
-
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-		MicrosoftService.authorizationCodeToToken();
 	}
-	
 }
