b6035d9b9f089d4236efcc71d45d3435e853e6d2
HyCraftHD <hycrafthd@live.de>
2021-05-18 20:19:23 +0200

    Add very basic auth file creator

--- src/main/java/net/hycrafthd/minecraft_authenticator/Main.java
+++ src/main/java/net/hycrafthd/minecraft_authenticator/Main.java
@@ -1,7 +1,71 @@
 package net.hycrafthd.minecraft_authenticator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import joptsimple.util.PathConverter;
+import joptsimple.util.PathProperties;
+import net.hycrafthd.minecraft_authenticator.login.AuthenticationException;
+import net.hycrafthd.minecraft_authenticator.login.Authenticator;
+import net.hycrafthd.minecraft_authenticator.microsoft.service.MicrosoftService;
+
 public class Main {
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
+		final OptionParser parser = new OptionParser();
+		
+		final OptionSpec<Void> helpSpec = parser.accepts("help", "Show the help menu").forHelp();
+		final OptionSpec<Path> authFileSpec = parser.accepts("auth-file", "Authentication file output").withRequiredArg().required().withValuesConvertedBy(new PathConverter(PathProperties.NOT_EXISTING));
+		
+		final OptionSet set = parser.parse(args);
+		
+		if (set.has(helpSpec) || set.specs().size() < 1) {
+			parser.printHelpOn(System.out);
+			return;
+		}
+		
+		final Path authFile = set.valueOf(authFileSpec);
+		
+		System.out.println("This setup creates a auth file for further authentication without user input");
+		
+		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			do {
+				System.out.println("Type 'microsoft' or 'mojang' for account type");
+				final String type = reader.readLine();
+				if (type.equals("microsoft")) {
+					System.out.println("Open the following link and log into your microsoft account");
+					System.out.println(MicrosoftService.oAuthLoginUrl());
+					System.out.println("Paste the code parameter of the returned url");
+					final String authCode = reader.readLine();
+					try {
+						Authenticator.of(authCode).shouldCreateAuthFile(authFile).build().run();
+					} catch (IOException | AuthenticationException ex) {
+						throw new IllegalStateException("An error occured while trying to create auth file", ex);
+					}
+					System.out.println("Successfully created microsoft auth file");
+					return;
+				} else if (type.equals("mojang")) {
+					System.out.println("Type in your username / email");
+					final String username = reader.readLine();
+					System.out.println("Type in your password");
+					final String password = reader.readLine();
+					System.out.println("Type in your client token (This will be used for every request and keeps you logged in)");
+					final String clientToken = reader.readLine();
+					
+					try {
+						Authenticator.of(clientToken, username, password).shouldCreateAuthFile(authFile).build().run();
+					} catch (IOException | AuthenticationException ex) {
+						throw new IllegalStateException("An error occured while trying to create auth file", ex);
+					}
+					System.out.println("Successfully created mojang auth file");
+					return;
+				}
+			} while (true);
+		}
 	}
 }
