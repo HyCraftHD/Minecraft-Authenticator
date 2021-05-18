19c2b4462a7bc8d4d617fa6ab8b74ba262d22a9a
HyCraftHD <hycrafthd@live.de>
2021-05-18 18:35:50 +0200

    Add authenticator, update authentication util

--- src/main/java/net/hycrafthd/minecraft_authenticator/login/Authenticator.java
+++ src/main/java/net/hycrafthd/minecraft_authenticator/login/Authenticator.java
@@ -1,38 +1,125 @@
 package net.hycrafthd.minecraft_authenticator.login;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-
-import net.hycrafthd.minecraft_authenticator.Constants;
+import net.hycrafthd.minecraft_authenticator.microsoft.MicrosoftLoginResponse;
+import net.hycrafthd.minecraft_authenticator.microsoft.MicrosoftLoginRoutine;
+import net.hycrafthd.minecraft_authenticator.util.AuthenticationUtil;
+import net.hycrafthd.minecraft_authenticator.yggdrasil.YggdrasilLoginResponse;
+import net.hycrafthd.minecraft_authenticator.yggdrasil.YggdrasilLoginRoutine;
 
 public class Authenticator {
 	
-	private static final Logger LOGGER = LogManager.getLogger();
-	
-	public static void writeAuthenticationFile(AuthenticationFile authFile, Path path) throws IOException {
-		final JsonElement element = Constants.GSON.toJsonTree(authFile);
-		if (element.isJsonObject()) {
-			element.getAsJsonObject().addProperty("warning", Constants.FILE_WARNING);
-		}
-		final String json = Constants.GSON_PRETTY.toJson(element);
-		Files.write(path, json.getBytes(StandardCharsets.UTF_8));
+	public static Builder of(AuthenticationFile file) {
+		return new Builder(() -> file);
 	}
 	
-	public static AuthenticationFile readAuthenticationFile(Path path) throws IOException {
-		try {
-			final String json = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
-			return Constants.GSON.fromJson(json, AuthenticationFile.class);
-		} catch (JsonParseException ex) {
-			throw new IOException("Cannot parse authentication file", ex);
+	public static Builder of(Path path) {
+		return new Builder(() -> AuthenticationUtil.readAuthenticationFile(path));
+	}
+	
+	public static Builder of(String authorizationCode) {
+		return new Builder(() -> AuthenticationUtil.createMicrosoftAuthenticationFile(authorizationCode));
+	}
+	
+	public static Builder of(String clientToken, String username, String password) {
+		return new Builder(() -> AuthenticationUtil.createYggdrasilAuthenticationFile(clientToken, username, password));
+	}
+	
+	public static final class Builder {
+		
+		private final AuthenticationFileSupplier file;
+		private Path createAuthFile;
+		private Path updateAuthFile;
+		private boolean authenticate;
+		
+		private Builder(AuthenticationFileSupplier file) {
+			this.file = file;
 		}
+		
+		public Builder shouldCreateAuthFile(Path path) {
+			createAuthFile = path;
+			return this;
+		}
+		
+		public Builder shouldUpdateAuthFile(Path path) {
+			updateAuthFile = path;
+			return this;
+		}
+		
+		public Builder shouldAuthenticate() {
+			authenticate = true;
+			return this;
+		}
+		
+		public Authenticator build() {
+			return new Authenticator(file, createAuthFile, updateAuthFile, authenticate);
+		}
+		
+	}
+	
+	private final AuthenticationFileSupplier file;
+	private final Path createAuthFile;
+	private final Path updateAuthFile;
+	private final boolean authenticate;
+	
+	public Authenticator(AuthenticationFileSupplier file, Path createAuthFile, Path updateAuthFile, boolean authenticate) {
+		this.file = file;
+		this.createAuthFile = createAuthFile;
+		this.updateAuthFile = updateAuthFile;
+		this.authenticate = authenticate;
+	}
+	
+	public Optional<User> run() throws IOException, AuthenticationException {
+		if (createAuthFile != null) {
+			AuthenticationUtil.writeAuthenticationFile(file.get(), createAuthFile);
+		}
+		if (authenticate) {
+			if (file instanceof AuthenticationFile.Microsoft) {
+				return runMicrosoftAuthentication((AuthenticationFile.Microsoft) file);
+			} else if (file instanceof AuthenticationFile.Yggdrasil) {
+				return runYggdrasilAuthentication((AuthenticationFile.Yggdrasil) file);
+			}
+		}
+		return Optional.empty();
+	}
+	
+	private Optional<User> runMicrosoftAuthentication(final AuthenticationFile.Microsoft microsoftFile) throws IOException, AuthenticationException {
+		final MicrosoftLoginResponse response = MicrosoftLoginRoutine.loginWithRefreshToken(microsoftFile.getRefreshToken());
+		
+		if (updateAuthFile != null && response.hasRefreshToken()) {
+			AuthenticationUtil.writeAuthenticationFile(new AuthenticationFile.Microsoft(response.getRefreshToken().get()), createAuthFile);
+		}
+		if (response.hasException()) {
+			throw response.getException().get();
+		}
+		if (!response.hasUser()) {
+			throw new AuthenticationException("After login there should be a user");
+		}
+		return response.getUser();
+	}
+	
+	private Optional<User> runYggdrasilAuthentication(final AuthenticationFile.Yggdrasil yggdrasilFile) throws IOException, AuthenticationException {
+		final YggdrasilLoginResponse response = YggdrasilLoginRoutine.loginWithAccessToken(yggdrasilFile.getAccessToken(), yggdrasilFile.getClientToken());
+		
+		if (updateAuthFile != null && response.hasAccessAndClientToken()) {
+			AuthenticationUtil.writeAuthenticationFile(new AuthenticationFile.Yggdrasil(response.getAccessToken().get(), response.getClientToken().get()), createAuthFile);
+		}
+		if (response.hasException()) {
+			throw response.getException().get();
+		}
+		if (!response.hasUser()) {
+			throw new AuthenticationException("After login there should be a user");
+		}
+		return response.getUser();
+	}
+	
+	@FunctionalInterface
+	private interface AuthenticationFileSupplier {
+		
+		AuthenticationFile get() throws IOException, AuthenticationException;
 	}
 	
 }
---
+++ src/main/java/net/hycrafthd/minecraft_authenticator/util/AuthenticationUtil.java
@@ -0,0 +1,66 @@
+package net.hycrafthd.minecraft_authenticator.util;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import net.hycrafthd.minecraft_authenticator.Constants;
+import net.hycrafthd.minecraft_authenticator.login.AuthenticationFile;
+import net.hycrafthd.minecraft_authenticator.microsoft.MicrosoftAuthenticationException;
+import net.hycrafthd.minecraft_authenticator.microsoft.api.OAuthErrorResponse;
+import net.hycrafthd.minecraft_authenticator.microsoft.api.OAuthTokenResponse;
+import net.hycrafthd.minecraft_authenticator.microsoft.service.MicrosoftResponse;
+import net.hycrafthd.minecraft_authenticator.microsoft.service.MicrosoftService;
+import net.hycrafthd.minecraft_authenticator.yggdrasil.YggdrasilAuthenticationException;
+import net.hycrafthd.minecraft_authenticator.yggdrasil.api.AuthenticatePayload;
+import net.hycrafthd.minecraft_authenticator.yggdrasil.api.AuthenticatePayload.Agent;
+import net.hycrafthd.minecraft_authenticator.yggdrasil.api.AuthenticateResponse;
+import net.hycrafthd.minecraft_authenticator.yggdrasil.service.YggdrasilResponse;
+import net.hycrafthd.minecraft_authenticator.yggdrasil.service.YggdrasilService;
+
+public class AuthenticationUtil {
+	
+	public static void writeAuthenticationFile(AuthenticationFile authFile, Path path) throws IOException {
+		final JsonElement element = Constants.GSON.toJsonTree(authFile);
+		if (element.isJsonObject()) {
+			element.getAsJsonObject().addProperty("warning", Constants.FILE_WARNING);
+		}
+		final String json = Constants.GSON_PRETTY.toJson(element);
+		Files.write(path, json.getBytes(StandardCharsets.UTF_8));
+	}
+	
+	public static AuthenticationFile readAuthenticationFile(Path path) throws IOException {
+		try {
+			final String json = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+			return Constants.GSON.fromJson(json, AuthenticationFile.class);
+		} catch (JsonParseException ex) {
+			throw new IOException("Cannot parse authentication file", ex);
+		}
+	}
+	
+	public static AuthenticationFile.Microsoft createMicrosoftAuthenticationFile(String authorizationCode) throws MicrosoftAuthenticationException {
+		final MicrosoftResponse<OAuthTokenResponse, OAuthErrorResponse> microsoftResponse = MicrosoftService.oAuthTokenFromCode(authorizationCode);
+		if (microsoftResponse.hasException()) {
+			throw new MicrosoftAuthenticationException("Cannot get oAuth token", microsoftResponse.getException().get());
+		} else if (microsoftResponse.hasErrorResponse()) {
+			throw new MicrosoftAuthenticationException("Cannot get oAuth token because: " + microsoftResponse.getErrorResponse().get());
+		}
+		final OAuthTokenResponse response = microsoftResponse.getResponse().get();
+		return new AuthenticationFile.Microsoft(response.getRefreshToken());
+	}
+	
+	public static AuthenticationFile.Yggdrasil createYggdrasilAuthenticationFile(String clientToken, String username, String password) throws YggdrasilAuthenticationException {
+		final YggdrasilResponse<AuthenticateResponse> yggdrasilResponse = YggdrasilService.authenticate(new AuthenticatePayload(new Agent("Minecraft", 1), username, password, clientToken, true));
+		if (yggdrasilResponse.hasException()) {
+			throw new YggdrasilAuthenticationException("Cannot authenticate minecraft account", yggdrasilResponse.getException().get());
+		} else if (yggdrasilResponse.hasErrorResponse()) {
+			throw new YggdrasilAuthenticationException("Cannot authenticate minecraft account because: " + yggdrasilResponse.getErrorResponse().get());
+		}
+		final AuthenticateResponse response = yggdrasilResponse.getResponse().get();
+		return new AuthenticationFile.Yggdrasil(response.getAccessToken(), response.getClientToken());
+	}
+}
