5fd0b619dbd22319e04a111193a72d0d0eac29c2
HyCraftHD <hycrafthd@live.de>
2021-06-30 20:54:59 +0200

    Add test for custom azure application

--- src/test/java/net/hycrafthd/minecraft_authenticator/test/CustomAzureApplicationMain.java
+++ src/test/java/net/hycrafthd/minecraft_authenticator/test/CustomAzureApplicationMain.java
@@ -1,9 +1,38 @@
 package net.hycrafthd.minecraft_authenticator.test;
 
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import net.hycrafthd.minecraft_authenticator.login.Authenticator;
+import net.hycrafthd.minecraft_authenticator.login.User;
+
 public class CustomAzureApplicationMain {
 	
-	public static void main(String[] args) {
+	private static final String AZURE_CLIENT_ID = "78590d64-3549-4c5f-9ef5-add1e816fed1";
+	private static final String AZURE_REDIRECT_PATH = "/ms-oauth/response";
+	private static final String AZURE_REDIRECT_URL = "http://localhost:{port}" + AZURE_REDIRECT_PATH;
+	
+	public static void main(String[] args) throws Exception {
+		final ServerSocket server = new ServerSocket(0);
+		final int port = server.getLocalPort();
+		final String redirectUrl = AZURE_REDIRECT_URL.replace("{port}", Integer.toString(port));
 		
+		Desktop.getDesktop().browse(Authenticator.microsoftLogin(AZURE_CLIENT_ID, redirectUrl).toURI());
+		
+		final Socket socket = server.accept();
+		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		final String getRequest = bufferedReader.readLine().split(" ")[1];
+		
+		final String authCode = getRequest.substring((AZURE_REDIRECT_PATH + "?code=").length());
+		
+		System.out.println(authCode);
+		
+		final Authenticator authenticator = Authenticator.ofMicrosoft(authCode).customAzureApplication(AZURE_CLIENT_ID, redirectUrl).shouldAuthenticate().run();
+		final User user = authenticator.getUser().get();
+		System.out.println(user);
 	}
 	
 }
