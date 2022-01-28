248272e0bd89f35d57378f5e21215ccafe54c164
HyCraftHD <hycrafthd@live.de>
2022-01-28 03:07:55 +0100

    Use try with resources

--- src/test/java/net/hycrafthd/minecraft_authenticator/test/CustomAzureApplicationMain.java
+++ src/test/java/net/hycrafthd/minecraft_authenticator/test/CustomAzureApplicationMain.java
@@ -15,25 +15,26 @@ public class CustomAzureApplicationMain {
 	private static final String AZURE_REDIRECT_URL = "http://localhost:{port}" + AZURE_REDIRECT_PATH;
 	
 	public static void main(String[] args) throws Exception {
-		final ServerSocket server = new ServerSocket(0);
-		final int port = server.getLocalPort();
-		final String redirectUrl = AZURE_REDIRECT_URL.replace("{port}", Integer.toString(port));
-		
-		System.out.println(Authenticator.microsoftLogin(AZURE_CLIENT_ID, redirectUrl));
-		
-		// Desktop.getDesktop().browse(Authenticator.microsoftLogin(AZURE_CLIENT_ID, redirectUrl).toURI());
-		
-		final Socket socket = server.accept();
-		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		final String getRequest = bufferedReader.readLine().split(" ")[1];
-		
-		final String authCode = getRequest.substring((AZURE_REDIRECT_PATH + "?code=").length());
-		
-		System.out.println(authCode);
-		
-		final Authenticator authenticator = Authenticator.ofMicrosoft(authCode).customAzureApplication(AZURE_CLIENT_ID, redirectUrl).shouldAuthenticate().run();
-		final User user = authenticator.getUser().get();
-		System.out.println(user);
+		try (ServerSocket server = new ServerSocket(0)) {
+			final int port = server.getLocalPort();
+			final String redirectUrl = AZURE_REDIRECT_URL.replace("{port}", Integer.toString(port));
+			
+			System.out.println(Authenticator.microsoftLogin(AZURE_CLIENT_ID, redirectUrl));
+			
+			// Desktop.getDesktop().browse(Authenticator.microsoftLogin(AZURE_CLIENT_ID, redirectUrl).toURI());
+			
+			final Socket socket = server.accept();
+			final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			final String getRequest = bufferedReader.readLine().split(" ")[1];
+			
+			final String authCode = getRequest.substring((AZURE_REDIRECT_PATH + "?code=").length());
+			
+			System.out.println(authCode);
+			
+			final Authenticator authenticator = Authenticator.ofMicrosoft(authCode).customAzureApplication(AZURE_CLIENT_ID, redirectUrl).shouldAuthenticate().run();
+			final User user = authenticator.getUser().get();
+			System.out.println(user);
+		}
 	}
 	
 }
