95cef4424132c1ff1367a07658dae2fb63b0cf68
HyCraftHD <hycrafthd@live.de>
2023-03-02 14:49:38 +0100

    Create module-info.info file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module minecraft_authenticator {
+	
+	exports net.hycrafthd.minecraft_authenticator;
+	exports net.hycrafthd.minecraft_authenticator.login;
+	
+	requires transitive com.google.gson;
+}
