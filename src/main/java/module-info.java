61c111233260a1bd2acdc24ba70faf915e4628eb
Johannes Jäger <hycrafthd@live.de>
2023-09-14 14:27:59 +0200

    Fix formatting

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module net.hycrafthd.minecraft_authenticator {
-
-    exports net.hycrafthd.minecraft_authenticator;
-    exports net.hycrafthd.minecraft_authenticator.login;
-    exports net.hycrafthd.minecraft_authenticator.util.function;
-
-    requires transitive com.google.gson;
-
-    opens net.hycrafthd.minecraft_authenticator.microsoft.api to com.google.gson;
+	
+	exports net.hycrafthd.minecraft_authenticator;
+	exports net.hycrafthd.minecraft_authenticator.login;
+	exports net.hycrafthd.minecraft_authenticator.util.function;
+	
+	requires transitive com.google.gson;
+	
+	opens net.hycrafthd.minecraft_authenticator.microsoft.api to com.google.gson;
 }
