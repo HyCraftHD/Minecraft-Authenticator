166abf8d02d4b38f3166373fdc67a0ad5c5229a3
Shirosaka <shiro@shirosaka.dev>
2023-09-14 14:28:50 +0200

    Fix OAuth authentication failing (#8)
    * fix: open the microsoft.api package to gson
    
    This is done so as to avoid an exception thrown by GSON being unable to find
    important types like 'OAuthTokenResponse', which causes authentication to fail
    with an exception.
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: Johannes Jäger <hycrafthd@live.de>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module net.hycrafthd.minecraft_authenticator {
 	exports net.hycrafthd.minecraft_authenticator.util.function;
 	
 	requires transitive com.google.gson;
+	
+	opens net.hycrafthd.minecraft_authenticator.microsoft.api to com.google.gson;
 }
